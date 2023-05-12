08f4f7132414360ec4fc0caeb2ba7268d0f7f630
Sebastian Post <sebastianpost@online.de>
2023-05-12 19:33:13 +0200

    initial release

--- projects/ngx-simple-signal/src/public-api.ts
+++ projects/ngx-simple-signal/src/public-api.ts
@@ -2,6 +2,4 @@
  * Public API Surface of ngx-simple-signal
  */
 
-export * from './lib/ngx-simple-signal.service';
-export * from './lib/ngx-simple-signal.component';
-export * from './lib/ngx-simple-signal.module';
+export * from './lib/simple-signal';
