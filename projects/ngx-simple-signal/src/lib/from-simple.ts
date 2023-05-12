8694cef36ea47185bc747a0e29ffee0d69e0b424
Sebastian Post <sebastianpost@online.de>
2023-05-12 21:28:26 +0200

    add fromSimple

---
+++ projects/ngx-simple-signal/src/lib/from-simple.ts
@@ -0,0 +1,15 @@
+import { Signal, computed } from '@angular/core';
+
+/**
+ * Converts a Simple Signal to a regular Signal.
+ * @param pointer A function pointing to the Simple Signal that should be converted.
+ * @returns A new Signal based off the Simple Signal.
+ * @example
+ * ```ts
+ *  ＠SimpleSignal(0) myNumber: number;
+ *  signal = fromSimple(() => this.myNumber);
+ * ```
+ */
+export function fromSimple<T>(pointer: () => T): Signal<T> {
+  return computed(() => pointer());
+}
