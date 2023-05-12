46244126f2049d68508d4782a179a5c05ec2e026
Sebastian Post <sebastianpost@online.de>
2023-05-12 19:31:34 +0200

    angular update

--- projects/ngx-simple-signal/src/lib/simple-signal.ts
+++ projects/ngx-simple-signal/src/lib/simple-signal.ts
@@ -1,16 +1,16 @@
-import { signal } from '@angular/core';
+import { signal, CreateSignalOptions } from '@angular/core';
 
-export function SimpleSignal<T>(value: T) {
+export function SimpleSignal<T>(value: T, options?: CreateSignalOptions<T>) {
   return function (target: object, key: string) {
     const privateKey = Symbol(key);
 
     Object.defineProperty(target, key, {
       set(newVal: T) {
-        this[privateKey] ??= signal(value);
+        this[privateKey] ??= signal(value, options);
         this[privateKey].set(newVal);
       },
       get() {
-        this[privateKey] ??= signal(value);
+        this[privateKey] ??= signal(value, options);
         return this[privateKey]();
       },
     });
